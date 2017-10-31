6befc320999cd4b4ee6874a554ba054479993abe
Gino Fiori <gino.fiori@gmail.com>
2017-10-31 20:02:23 -0300

    ADD

---
+++ includes/valores_menu2.php
@@ -0,0 +1,6 @@
+<?php
+  $menu = array();
+  $menu['Home'] = array('index.php' , 'fa fa-home');
+  $menu['Acerca Nuestro'] = array('acerca-nuestro.php', 'fa fa-info-circle');
+  $menu['Login'] = array('iniciar-sesion.php', 'fa fa-user-circle-o');
+?>
